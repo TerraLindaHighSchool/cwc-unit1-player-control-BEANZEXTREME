6c0a57b6c7fbd69db77667f5ea9d9fe452c41cbc
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2020-02-04 15:31:15 -0800

    Challenge 2.7
    I might have done it/????

--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,17 +16,21 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
+       // InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        Invoke("SpawnRandomBall", spawnInterval);
+        
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[Random.Range(0,3)], spawnPos, ballPrefabs[Random.Range(0, 3)].transform.rotation);
+        Instantiate(ballPrefabs[Random.Range(0,3)], spawnPos, ballPrefabs[Random.Range(0,3)].transform.rotation);
+        spawnInterval = Random.Range(1,6);
     }
 
 }
