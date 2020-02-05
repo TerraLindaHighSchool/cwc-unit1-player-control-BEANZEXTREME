120bd08012b7bd8ddd3cdc8efc3f11ad41a8cba9
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2020-02-04 16:12:55 -0800

    Challenge 2.8
    wow really hard

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    private float oldTime = 0;
+    private bool firstTime = true;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)  && (((Time.time - oldTime) >= .5f) || firstTime))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            oldTime = Time.time;
+            firstTime = false;
         }
     }
 }
